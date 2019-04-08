1f6bec5f7467fa96a8deb4e8485a3e60dd08e5c5
algprasad <algprasad@outlook.com>
2019-04-08 20:03:39 +0100

    added cmake-build-debug files

---
+++ cmake-build-debug/CMakeFiles/isam2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/isam2.dir/src/isam2.cpp.o"
+  "devel/lib/isam2/isam2.pdb"
+  "devel/lib/isam2/isam2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/isam2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/socrob/isam2_ws/src/isam2
+# Build directory: /home/socrob/isam2_ws/src/isam2/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
