ff8dcbf27920be9d933e76f17bd067df7c397217
algprasad <algprasad@outlook.com>
2019-04-08 19:56:14 +0100

    created package

---
+++ src/isam2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "ros/ros.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    //ros::init(argc, argv, "isam2");
+    //ros::NodeHandle nh;
+
+    //Publishing and Subscribing
+
+    //ros::Rate rate(20);
+    /*while(ros::ok()) {
+
+
+
+        //publish the data
+
+
+        ros::spinOnce();
+        rate.sleep();
+    }*/
+
+    return 0;
+
+}
